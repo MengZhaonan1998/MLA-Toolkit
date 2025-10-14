77bbda4958a134a5507f959c122abf14d3bdc81c
MengZhaonan1998 <mengzn1998@gmail.com>
2025-10-13 23:01:34 -0400

    remove TTID_PRRLU_FORWARD, already legacy

--- py/tt_id.py
+++ py/tt_id.py
@@ -36,37 +36,6 @@ def TT_IDPRRLDU(tensorX: tl.tensor, r_max: int, eps: float, verbose: int = 0) ->
     ttList.reverse()
     return ttList
 
-# TT-ID-PRRLU Forward sweep
-def TT_IDPRRLDU_Forward(tensorX: tl.tensor, r_max: int, eps: float, verbose: int = 0) -> list[tl.tensor]:
-    shape = tensorX.shape  # Get the shape of input tensor: [n1, n2, ..., nd]
-    dim = len(shape)       # Get the number of dimension
-    delta = (eps / np.sqrt(dim - 1)) * tl.norm(tensorX, 2)  # Truncation parameter
-    
-    W = tensorX        # Copy tensor X -> W
-    nbar = W.size      # Total size of W
-    r = 1              # Rank r
-    ttList = []        # list storing tt factors
-    iterlist = list(range(0, dim-1))  # Create iteration list: 1, 2, ..., d-1
-    
-    for i in iterlist:
-        W = tl.reshape(W, [int(r * shape[i]), int(nbar / r / shape[i])])  # Reshape W       
-        C, X, cols, error = interpolative_prrldu(W, cutoff=delta, maxdim=r_max)
-        ri = X.shape[0]  # r_i-1 = min(r_max, r_delta_i)
-    
-        if verbose == 1:
-            rerror = tl.norm(C @ X - W, 2) / tl.norm(W, 2)
-            print(f"Iteration {i} -- low rank id approximation error = {rerror}")
-    
-        Ti = tl.reshape(C[:, 0:ri], [r, shape[i], ri])
-        nbar = int(nbar * ri / shape[i] / r)  # New total size of W
-        r = ri             # Renewal r
-        W = X[0:ri, :]     # W = U[..] * S[..]
-        ttList.append(Ti)  # Append new factor
-    
-    T1 = tl.reshape(W, [r, shape[dim-1], 1])
-    ttList.append(T1)    
-    return ttList
-
 # TT-ID based on nuclear score
 def TT_IDscatter(tensorX: tl.tensor, r_max: int, eps: float, verbose: int = 0) -> list[tl.tensor]:
     shape = tensorX.shape  # Get the shape of input tensor: [n1, n2, ..., nd]
